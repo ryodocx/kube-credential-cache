96433756cdad41f2255aeb2e8867ec28699701c9
ryodocx <email@ryodocx.net>
2022-08-12 07:27:17 +0900

    rename ENV

--- config.go
+++ config.go
@@ -13,21 +13,21 @@ var (
 )
 
 func init() {
-	if e := os.Getenv("EKS_TOKEN_CACHE_FILE"); e != "" {
+	if e := os.Getenv("K8S_TOKEN_CACHE_FILE"); e != "" {
 		cacheFilepath = e
 	} else {
 		cacheDir, err := os.UserCacheDir()
 		if err != nil {
-			log.Fatalf("can't find CacheDir. fix error or set 'EKS_TOKEN_CACHE_FILE': %s", err)
+			log.Fatalf("can't find CacheDir. fix error or set 'K8S_TOKEN_CACHE_FILE': %s", err)
 		}
 		cacheFilepath = path.Join(cacheDir, "k8s-token-cache/token.json")
 		// mac: /Users/${USER}/Library/Caches/k8s-token-cache/token.json
 	}
 
-	if e := os.Getenv("EKS_TOKEN_CACHE_REFRESH_MARGIN"); e != "" {
+	if e := os.Getenv("K8S_TOKEN_CACHE_REFRESH_MARGIN"); e != "" {
 		d, err := time.ParseDuration(e)
 		if err != nil {
-			log.Fatalf("invalid environment variable 'EKS_TOKEN_CACHE_REFRESH_MARGIN': %s", err.Error())
+			log.Fatalf("invalid environment variable 'K8S_TOKEN_CACHE_REFRESH_MARGIN': %s", err.Error())
 		}
 		refreshMargin = d
 	}
